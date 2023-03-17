951332ec5c4d58a062e79b281ea40ff4f54c24e6
Mohid123 <muhammadmohid141@gmail.com>
2023-03-17 21:25:48 +0500

    configured app as pwa and created service worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { ApplicationRef, Component } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { concat, filter, first, interval } from 'rxjs';
 
-// function promptUser(event: VersionReadyEvent): boolean {
-//   return true;
-// }
+function promptUser(event: VersionReadyEvent): boolean {
+  return true;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,26 +12,26 @@ import { concat, filter, first, interval } from 'rxjs';
 })
 export class AppComponent {
   title = 'User_Manager';
-  // constructor(appRef: ApplicationRef, swUpdate: SwUpdate) {
-  //   const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
-  //   const everySixHours$ = interval(6 * 60 * 60 * 1000);
-  //   const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
-  //   everySixHoursOnceAppIsStable$.subscribe(async () => {
-  //     try {
-  //       const updateFound = await swUpdate.checkForUpdate();
-  //       console.log(updateFound ? 'A new version is available.' : 'Already on the latest version.');
-  //     } catch (err) {
-  //       console.error('Failed to check for updates:', err);
-  //     }
-  //   });
+  constructor(appRef: ApplicationRef, swUpdate: SwUpdate) {
+    const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true));
+    const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+    everySixHoursOnceAppIsStable$.subscribe(async () => {
+      try {
+        const updateFound = await swUpdate.checkForUpdate();
+        console.log(updateFound ? 'A new version is available.' : 'Already on the latest version.');
+      } catch (err) {
+        console.error('Failed to check for updates:', err);
+      }
+    });
 
-  //   swUpdate.versionUpdates
-  //   .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
-  //   .subscribe(evt => {
-  //     if (promptUser(evt)) {
-  //       // Reload the page to update to the latest version.
-  //       document.location.reload();
-  //     }
-  //   });
-  // }
+    swUpdate.versionUpdates
+    .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+    .subscribe(evt => {
+      if (promptUser(evt)) {
+        // Reload the page to update to the latest version.
+        document.location.reload();
+      }
+    });
+  }
 }
